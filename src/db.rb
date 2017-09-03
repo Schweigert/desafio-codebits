2145d050b86365c57a9b20c92cef0efe2b9ac1e3
Marlon Henry Schweigert <fleyhe0@gmail.com>
2017-09-02 23:31:05 -0300

    Banco de Dados (MySql2 gem)

---
+++ src/db.rb
@@ -0,0 +1,15 @@
+require 'mysql2'
+
+require_relative "config"
+
+# Esta classe representa um objeto com conexão ao banco de dados.
+# Todo objeto que requer uma conexão ao banco deve herdar essa classe.
+class Db
+
+  # Inicializa a conexão com o banco de dados
+  @@connection = Mysql2::Client.new :host     => Config.dbHost,
+                                    :database => Config.dbName,
+                                    :username => Config.dbUser,
+                                    :password => Config.dbPass
+
+end
